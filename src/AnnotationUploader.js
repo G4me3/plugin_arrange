6be2bc7ad521b4f1473a10c12ab46bf167761f2d
G4me3 <84765307+G4me3@users.noreply.github.com>
2024-07-02 17:17:31 +0900

    modify comment

--- src/AnnotationUploader.js
+++ src/AnnotationUploader.js
@@ -1,24 +1,23 @@
 function uploadAnnotations(content, username, delete_key) {
-    console.log(username);
     setUsernameToLocalStorage(username);
-    console.log(content);
-    console.log(delete_key);
+    setDeleteKeytoLocalStorage(delete_key);
 
     let currentDate = new Date();
     const year = currentDate.getFullYear(); // 年
     const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 月
     const day = String(currentDate.getDate()).padStart(2, '0'); // 日
-    const hours = String(currentDate.getHours()).padStart(2, '0'); // 時（0埋め）
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0'); // 分（0埋め）
-    const formattedDate = `${year}/${month}/${day} ${hours}:${minutes}`;
+    // const hours = String(currentDate.getHours()).padStart(2, '0'); // 時（0埋め）
+    // const minutes = String(currentDate.getMinutes()).padStart(2, '0'); // 分（0埋め）
+    const formattedDate = `${year}/${month}/${day}`;
 
     //ローカルストレージのアノテーションを実際のJSONに組み込めるように値を調整
     for (let item of content.items) {
         //<p>タグ削除とタイムスタンプ、氏名の付与
         item.body.value = item.body.value.replace(/<p[^>]*>|<\/p>/g, "");
-        item.body.value = "<p>" + formattedDate + "  " + username + "</p>" + item.body.value;
+        item.body.value = "<p>" + formattedDate + "  " + username + " (サーバ上)</p>" + item.body.value;
     }
 
+    content["delete_key"] = delete_key;
 
     fetch("https://www.sugilab.net/mori.hiroyuki.lab/sotuken/manifests/uploadAnnotation.php", {
         method: 'POST',
@@ -59,4 +58,8 @@ export function setUsernameToTextBox() {
     const username = getUsernameFromLocalstorage();
     const usernameTextbox = document.getElementById("username");
     usernameTextbox.value = username;
+}
+
+function setDeleteKeytoLocalStorage(delete_key) {
+    localStorage.setItem("delete_key", delete_key);
 }
